6df8ab6aa7dd332d3c75615c61e98a649a6900a1
Berkay Aytekin <berkay.0567@gmail.com>
2025-04-22 13:11:42 +0300

    #1

---
+++ server/config/rolePrivileges.js
@@ -0,0 +1,100 @@
+module.exports = {
+  privGroups: [
+    {
+      id: "USERS",
+      name: "User Permissions",
+    },
+    {
+      id: "ROLES",
+      name: "User Permissions",
+    },
+    {
+      id: "CATEGORİES",
+      name: "User Permissions",
+    },
+    {
+      id: "AUDITLOGS",
+      name: "User Permissions",
+    },
+  ],
+  privileges: [
+    {
+      key: "user_view",
+      name: "User view",
+      group: "USERS",
+      description: "User view",
+    },
+    {
+      key: "user_add",
+      name: "User add",
+      group: "USERS",
+      description: "User add",
+    },
+    {
+      key: "user_update",
+      name: "User update",
+      group: "USERS",
+      description: "User update",
+    },
+    {
+      key: "user_delete",
+      name: "User delete",
+      group: "USERS",
+      description: "User delete",
+    },
+    {
+      key: "role_view",
+      name: "Role view",
+      group: "ROLES",
+      description: "Role view",
+    },
+    {
+      key: "role_add",
+      name: "Role add",
+      group: "ROLES",
+      description: "Role add",
+    },
+    {
+      key: "role_update",
+      name: "Role update",
+      group: "ROLES",
+      description: "Role update",
+    },
+    {
+      key: "role_delete",
+      name: "Role delete",
+      group: "ROLES",
+      description: "Role delete",
+    },
+    {
+      key: "category_view",
+      name: "Category view",
+      group: "CATEGORİES",
+      description: "Category view",
+    },
+    {
+      key: "category_add",
+      name: "Category add",
+      group: "CATEGORİES",
+      description: "Category add",
+    },
+    {
+      key: "category_update",
+      name: "Category update",
+      group: "CATEGORİES",
+      description: "Category update",
+    },
+    {
+      key: "category_delete",
+      name: "Category delete",
+      group: "CATEGORİES",
+      description: "Category delete",
+    },
+    {
+      key: "auditlogs_view",
+      name: "AuditLogs delete",
+      group: "AUDITLOGS",
+      description: "AuditLogs view",
+    },
+  ],
+};
--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,13 +1,92 @@
 const express = require("express");
+const Categories = require("../db/models/Categories");
+const Response = require("../lib/Response");
+const CustomError = require("../lib/Error");
+const Enum = require("../config/Enum");
+const AuditLogs = require("../lib/AuditLogsLib");
+const logger = require("../lib/logger/LoggerClass");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    body: req.body,
-    params: req.params,
-    query: req.query,
-    headers: req.headers,
-  });
+router.get("/", async (req, res) => {
+  try {
+    let categories = await Categories.find({});
+    res.json(Response.successResponse(categories));
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(Response.errorResponse(err));
+  }
+});
+
+router.post("/add", async (req, res) => {
+  let body = req.body;
+  try {
+    if (!body.name)
+      throw new CustomError(
+        Enum.HTTP_CODES.BAD_REQUEST,
+        "Validation error",
+        "name field must be filled"
+      );
+    let category = new Categories({
+      name: body.name,
+      is_active: true,
+      created_by: req.user?.id,
+    });
+
+    await category.save();
+
+    AuditLogs.info(req.user?.email, "Categories", "Add", category);
+    logger.info(req.user?.email, "Categories", "Add", category);
+
+    res.json(Response.successResponse({ success: true }));
+  } catch (err) {
+    logger.error(req.user?.email, "Categories", "Add", err);
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(Response.errorResponse(err));
+  }
+});
+
+router.post("/update", async (req, res) => {
+  let body = req.body;
+  try {
+    if (!body._id)
+      throw new CustomError(
+        Enum.HTTP_CODES.BAD_REQUEST,
+        "Validation Error",
+        "_id field must be filled"
+      );
+    let updates = {};
+    if (body.name) updates.name = body.name;
+    if (typeof body.is_active === "boolean") updates.is_active = body.is_active;
+    await Categories.updateOne({ _id: body._id }, updates);
+    AuditLogs.info(req.user?.email, "Categories", "Update", {
+      _id: body._id,
+      ...updates,
+    });
+
+    res.json(Response.successResponse({ success: true }));
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(Response.errorResponse(err));
+  }
+});
+
+router.post("/delete", async (req, res) => {
+  let body = req.body;
+  try {
+    if (!body._id)
+      throw new CustomError(
+        Enum.HTTP_CODES.BAD_REQUEST,
+        "Validation Error",
+        "_id field must be filled"
+      );
+    await Categories.deleteOne({ _id: body._id });
+    AuditLogs.info(req.user?.email, "Categories", "Add", "Added");
+    AuditLogs.info(req.user?.email, "Categories", "Delete", { _id: body._id });
+    res.json(Response.successResponse({ success: true }));
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(Response.errorResponse(err));
+  }
 });
 
 module.exports = router;
