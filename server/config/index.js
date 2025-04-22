b98ba031d8469f024cbf6f8f7e9d01914747b075
Berkay Aytekin <berkay.0567@gmail.com>
2025-04-22 19:29:32 +0300

    #7 User Auth kuruldu

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,12 @@
+/* eslint-disable no-undef */
 module.exports = {
   PORT: process.env.PORT || "3000",
   LOG_LEVEL: process.env.LOG_LEVEL || "debug",
   DATABASE_CONNECTION: process.env.DATABASE_CONNECTION,
+  JWT: {
+    SECRET: "123456",
+    EXPIRE_TIME: !isNaN(parseInt(process.env.TOKEN_EXPIRE_TIME))
+      ? parseInt(process.env.TOKEN_EXPIRE_TIME)
+      : 24 * 60 * 60,
+  },
 };
