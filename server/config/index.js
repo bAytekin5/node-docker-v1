4354a7655becafe083463f34fbca34bbd48fb13c
Berkay Aytekin <berkay.0567@gmail.com>
2025-05-02 00:45:38 +0300

    #27

--- server/config/index.js
+++ server/config/index.js
@@ -4,7 +4,7 @@ module.exports = {
   LOG_LEVEL: process.env.LOG_LEVEL || "debug",
   DATABASE_CONNECTION: process.env.DATABASE_CONNECTION,
   JWT: {
-    SECRET: "123456",
+    SECRET: process.env.JWT_SECRET || "",
     EXPIRE_TIME: !isNaN(parseInt(process.env.TOKEN_EXPIRE_TIME))
       ? parseInt(process.env.TOKEN_EXPIRE_TIME)
       : 24 * 60 * 60,
